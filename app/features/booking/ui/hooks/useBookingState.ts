167b8b4c70dd0c9c1ba97b3bd4820148c82f788e
Owl0_0 <prechencka473@gmail.com>
2026-02-24 19:50:02 +0300

    Stated to making the component

---
+++ app/features/booking/ui/hooks/useBookingState.ts
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { BookingFormData } from "../../model/booking.types";
+
+export const useBookingState = () => {
+    const [data, setData] = useState<BookingFormData | null>(null);
+    const [open, setOpen] = useState(false);
+
+    return {
+        open,
+        data,
+        openModal: () => setOpen(true),
+        closeModal: () => setOpen(false),
+        submit: (data: BookingFormData) => setData(data),
+    };
+}
