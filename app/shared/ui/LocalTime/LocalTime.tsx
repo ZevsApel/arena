89ac255edd6ad799c1d44a51155e808a946e554d
Owl0_0 <pechencka473@gmail.com>
2026-03-26 10:09:42 +0300

    the local time component in Makhachkala has been added

---
+++ app/shared/ui/LocalTime/LocalTime.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
+const LocalTime = () => {
+    const [time, setTime] = useState('');
+
+    useEffect(() => {
+        const updateTime = () => {
+            const formatter = new Intl.DateTimeFormat("ru-RU", {
+                timeZone: "Europe/Moscow",
+                hour: "2-digit",
+                minute: "2-digit"
+            });
+
+            setTime(formatter.format(new Date()));
+        }
+
+        updateTime();
+        const interval =setInterval(updateTime, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="local-time">
+            Местное время: {time}
+        </div>
+    );
+}
+
+export default LocalTime;
